8e12ce918b02902e2ec2caee475a56036bf4f28e
Suresh kanchi <soorihai@gmail.com>
2025-05-06 21:21:48 +0530

    Delete src directory

--- src/config/env.ts
+++
@@ -1,4 +0,0 @@
-// Environment configuration
-export const IS_DEVELOPMENT = import.meta.env.MODE === 'development';
-export const IS_PRODUCTION = import.meta.env.MODE === 'production';
-export const IS_PREVIEW = window.location.port === '4173'; 
